239cd2267fbc4602f3b5addd8c672ca5fa52519f
akash110826 <25113cn310@glbitm.ac.in>
2026-05-28 23:56:18 +0530

    Create alphabetprint.c

---
+++ patternprint.c
@@ -0,0 +1,17 @@
+//wap to print pattaer
+//A B C D
+//A B C D
+//A B C D
+//A B C D
+#include<stdio.h>
+int main()
+{
+    int n ,i, j  ;
+    printf("Enter the hight");
+    scanf("%d",&n);
+    for( i=1 ; i<=n ; i++)
+    {
+        char 
+    }
+    return 0 ;
+}
